c331205a3e18a44b142496a582ef338aaec4960c
YANGTA2 <y1103at@gmail.com>
2021-12-06 23:20:01 +0900

    21-12-06

---
+++ src/main/java/com/BAEKJOON/IF/BAEKJOON14681.java
@@ -0,0 +1,29 @@
+package com.BAEKJOON.IF;
+
+import java.util.Scanner;
+
+public class BAEKJOON14681 {
+    public static void main(String[] args) {
+
+        BAEKJOON14681 sol = new BAEKJOON14681();
+        System.out.println(sol.solution());
+    }
+
+    public int solution(){
+        
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        int result = 0;
+
+        if(x > 0){
+            result = (y > 0) ? 1 :  4;
+        } else {
+            result = (y > 0) ? 2 :  3;
+        }
+
+        return result;
+    }
+}
