476fc5ae7cea45c75781c827df9010ec2b186f1e
YANGTA2 <y1103at@gmail.com>
2021-12-15 22:39:57 +0900

    21-12-15 : 오늘의 스터디

--- src/main/java/com/BAEKJOON/OneDimentionalArray/BAEKJOON8958.java
+++ src/main/java/com/BAEKJOON/OneDimentionalArray/BAEKJOON8958.java
@@ -15,16 +15,15 @@ public class BAEKJOON8958 {
         
         for(int i = 0; i < n; i++){ 
             String result = sc.next();
-            String[] spl = result.split("X");
+            String[] split = result.split("X");
             
             int totalScore = 0;
-            for (String str : spl) {
+            for (String str : split) {
                 int score = (str.length() > 0) ? (str.length()+1)*str.length()/2 : 0;
                 totalScore += score;
             }
             System.out.println(totalScore);
         }
-
         sc.close();
     }
 }
