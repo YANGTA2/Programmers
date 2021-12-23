916555f61d48734905358541381a3c542cec500d
YANGTA2 <y1103at@gmail.com>
2021-12-23 17:10:11 +0900

    21-12-23 : 문자열 완료

--- src/main/java/com/BAEKJOON/String/BAEKJOON1316.java
+++ src/main/java/com/BAEKJOON/String/BAEKJOON1316.java
@@ -1,5 +1,6 @@
 package com.BAEKJOON.String;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class BAEKJOON1316 {
@@ -13,8 +14,36 @@ public class BAEKJOON1316 {
     public int solution(){
 
         Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
         int result = 0;
+        
+        for(int a = 0; a < n; a++){
+            int cnt = 0;
+            String word = sc.nextLine();
+            HashMap<String, Integer> map = new HashMap<>();
+            
+            for(int i = 0; i < word.length(); i++){
 
+                String now = String.valueOf(word.charAt(i));
+                
+                if(i != word.length()-1){
+                    String next = String.valueOf(word.charAt(i+1));
+                    if(!now.equals(next)){
+                        map.put(now, map.getOrDefault(now, 0)+1);
+                        cnt++;
+                    }
+                } else {
+                    map.put(now, map.getOrDefault(now, 0)+1);
+                    cnt++;
+                }
+                if(map.size() != cnt){
+                    break;
+                };
+            }
+            if(map.size() == cnt){
+                result++;
+            };
+        }
         sc.close();
         return result;
     }
