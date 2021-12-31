30e6aec01bda03bb24fe6a74ea44a424ba29f1ff
YANGTA2 <y1103at@gmail.com>
2021-12-31 19:12:01 +0900

    21-12-31 : 기본 수학 1 완료

---
+++ src/main/java/com/BAEKJOON/BasicMath1/BAEKJOON1011.java
@@ -0,0 +1,41 @@
+package com.BAEKJOON.BasicMath1;
+
+import java.util.Scanner;
+
+public class BAEKJOON1011 {
+    
+    public static void main(String[] args) {
+        
+        BAEKJOON1011 sol = new BAEKJOON1011();
+        sol.solution();
+    }
+    
+    public void solution(){
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        for(int i = 0; i < t; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            int distance = y - x;
+            Double validate = Math.sqrt((double)distance);
+
+            int result = 0;
+            if(validate % 1 == 0){
+                result = (int)(validate * 2 - 1);
+            } else {
+                Double temp = Math.ceil(validate);
+                if(distance > Math.pow(temp, 2) - temp){
+                    result = (int)(temp * 2 - 1);
+                } else {
+                    result = (int)(temp * 2 - 2);
+                }
+            }
+            System.out.println(result);
+        }
+
+        sc.close();
+    }
+}
